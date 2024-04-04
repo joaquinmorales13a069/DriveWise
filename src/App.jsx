9c045ba063e42f3648a47b9c90fbd35f7c16ea4e
Joaquin Morales <joaquinmorales13a06@gmail.com>
2024-04-05 02:01:21 +1100

    Created and customized Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    <main className="font-roboto">
+      <Navbar />
+    </main>
   )
 }
